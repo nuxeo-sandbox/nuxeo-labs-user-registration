7b60445480e0590921516fb7030b82f355a7a961
michael <mvachette@nuxeo.com>
2019-05-14 12:42:29 +0900

    Add self registration endpoint

---
+++ nuxeo-labs-user-registration-core/src/main/java/org/nuxeo/user/registration/endpoint/UserRegistrationObject.java
@@ -0,0 +1,63 @@
+package org.nuxeo.user.registration.endpoint;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.nuxeo.ecm.automation.AutomationService;
+import org.nuxeo.ecm.automation.OperationChain;
+import org.nuxeo.ecm.automation.OperationContext;
+import org.nuxeo.ecm.automation.OperationException;
+import org.nuxeo.ecm.core.api.CloseableCoreSession;
+import org.nuxeo.ecm.core.api.CoreInstance;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.NuxeoException;
+import org.nuxeo.ecm.webengine.model.WebObject;
+import org.nuxeo.ecm.webengine.model.impl.ModuleRoot;
+import org.nuxeo.runtime.api.Framework;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+/**
+ * WebEngine module to handle the self-service registration
+ */
+@Path("/selfregistration")
+@WebObject(type = "selfregistration")
+public class UserRegistrationObject extends ModuleRoot {
+
+    protected static final Log log = LogFactory.getLog(UserRegistrationObject.class);
+
+    public static final String PATH = "selfregistration";
+
+    @Path("/")
+    @POST
+    public Object doPost(@Context HttpServletRequest request) throws IOException {
+        InputStream in = request.getInputStream();
+        String json = IOUtils.toString(in, Charset.defaultCharset());
+        AutomationService as = Framework.getService(AutomationService.class);
+        CoreSession session = CoreInstance.openCoreSessionSystem(null);
+        try {
+            OperationContext ctx = new OperationContext();
+            ctx.setCoreSession(session);
+            OperationChain chain = new OperationChain("processUserRegistrationRequest");
+            chain.add("javascript.api_new_account_request").
+                    set("request",json);
+            as.run(ctx, chain);
+            return Response.status(Response.Status.OK).build();
+        } catch (OperationException e) {
+            throw new NuxeoException(e);
+        } finally {
+            if (session != null) {
+                ((CloseableCoreSession) session).close();
+                session = null;
+            }
+        }
+    }
+
+}
---
+++ nuxeo-labs-user-registration-core/src/test/java/org/nuxeo/user/registration/TestRegistrationEndpoint.java
@@ -0,0 +1,48 @@
+package org.nuxeo.user.registration;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nuxeo.ecm.automation.test.AutomationFeature;
+import org.nuxeo.ecm.core.test.annotations.Granularity;
+import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
+import org.nuxeo.ecm.restapi.test.RestServerFeature;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.ServletContainerFeature;
+import org.nuxeo.user.registration.endpoint.UserRegistrationObject;
+
+import javax.inject.Inject;
+import java.io.IOException;
+
+@RunWith(FeaturesRunner.class)
+@Features({AutomationFeature.class, RestServerFeature.class})
+@RepositoryConfig(cleanup = Granularity.METHOD)
+@Deploy({
+        "nuxeo-labs-user-registration-core",
+        "nuxeo-labs-user-registration-core:OSGI-INF/default-chain-contrib.xml"
+})
+public class TestRegistrationEndpoint {
+
+    public static final String BASE_URL = "http://localhost";
+
+    @Inject
+    protected ServletContainerFeature servletContainerFeature;
+
+    @Test
+    public void testReceiveRegistrationCall() throws IOException {
+        int port = servletContainerFeature.getPort();
+        String url = BASE_URL + ":"+port+"/"+ UserRegistrationObject.PATH;
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpPost post = new HttpPost(url);
+        //post.setEntity(new InputStreamEntity(getClass().getResourceAsStream("/files/callback.xml")));
+        HttpResponse response = client.execute(post);
+        Assert.assertEquals(200,response.getStatusLine().getStatusCode());
+    }
+
+}
